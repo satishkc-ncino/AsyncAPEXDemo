8cdc550358c28175717a44b294cac2f615b85d64
Satish Chandrashekar <satish.chandrashekar@ncino.com>
2021-03-15 17:07:53 +1100

    updated

---
+++ .sfdx/typings/lwc/apex/ContinuationClass.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ContinuationClass.startRequest" {
+  export default function startRequest(): Promise<any>;
+}
+declare module "@salesforce/apex/ContinuationClass.processResponse" {
+  export default function processResponse(param: {labels: any, state: any}): Promise<any>;
+}
